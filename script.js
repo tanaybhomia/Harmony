9ff853d6a4fea1e748a4fe5c3893b9aa5ce1b1d8
Tanay Bhomia <bhomia123456789@gmail.com>
2024-01-05 08:13:57 +0530

    Added test functions

--- script.js
+++ script.js
@@ -16,14 +16,27 @@ function transitionGradient() {
       }
     }
 
-    const saturation = 70; // Adjust saturation (0 to 100)
-    const lightness = 70; // Adjust lightness (0 to 100)
+    const saturation = 50; // Adjust saturation (0 to 100)
+    const lightness = 60; // Adjust lightness (0 to 100)
 
     const color = `hsl(${hue}, ${saturation}%, ${lightness}%)`; // Use HSL color format for smooth transitions
 
     document.body.style.background = color;
   }, 30); // Change color every 30 milliseconds (adjust as needed)
 }
-
 // Call the function when the page loads
 window.onload = transitionGradient;
+
+function funcmute() {
+  console.log("A button test");
+}
+function profileredirect() {
+  const url = "https://github.com/tanaybhomia";
+  window.location.href = url;
+}
+
+const mute = document.querySelector(".muting");
+mute.addEventListener("click", funcmute);
+
+const profile = document.querySelector(".github");
+profile.addEventListener("click", profileredirect);
